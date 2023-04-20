ace41f526166f023a86a14b0ef6cb8ff185d5f1d
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-04-20 14:37:01 +0200

    Task Methode(CountineWith)

---
+++ TaskKonsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+Console.WriteLine("Hello, World!");
+
+#region Task mit ContinueWith 
+//var meineTask =new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((daten)=>
+//{
+//    Console.WriteLine("Datengrösse:"+ daten.Result.Length);
+//});
+//Console.WriteLine("Zwischenarbeiten");
+//await meineTask;
+#endregion
+
+#region Task ohne ContinueWith (Best practices)
+var meineTask = new HttpClient().GetStringAsync("https://www.google.com");
+Console.WriteLine("Zwischenarbeiten");
+
+var daten = await meineTask;
+Console.WriteLine("Datengrösse:" + daten.Length);
+
+#endregion
