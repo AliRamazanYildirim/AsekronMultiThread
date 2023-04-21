39d12f88a4cf6e553dba565596ac953327f8b46c
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-04-21 22:31:09 +0200

    Task Methode(WhenAll)

--- TaskKonsoleApp/Program.cs
+++ TaskKonsoleApp/Program.cs
@@ -1,7 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+namespace TaskKonsoleApp
+{
+    public class Inhalt
+    {
+        public string? Seite { get; set; }
+        public int Länge { get; set; }
+
+    }
+    internal class Programm
+    {
+        static async Task Main(string[] args)
+        {
+            await Console.Out.WriteLineAsync("Haupt Thread:" + Thread.CurrentThread.ManagedThreadId);
+            List<string> urlListe = new List<string>()
+            {
+                 "https://www.google.com",
+                 "https://www.microsoft.com",
+                 "https://www.amazon.com",
+                 "https://www.apple.com"
+            };
+
+            List<Task<Inhalt>> taskList = new List<Task<Inhalt>>();
+            urlListe.ToList().ForEach(x =>
+            {
+                taskList.Add(RufeInhaltAufAsync(x));
+            });
+
+            var inhalte = await Task.WhenAll(taskList.ToArray());
+
+            inhalte.ToList().ForEach(x =>
+            {
+                Console.WriteLine($"{x.Seite}- Grösse:{x.Länge}");
+            });
+            static async Task<Inhalt> RufeInhaltAufAsync(string url)
+            {
+                Inhalt inhalt = new Inhalt();
+                var daten = await new HttpClient().GetStringAsync(url);
+
+                inhalt.Seite = url;
+                inhalt.Länge = daten.Length;
+                await Console.Out.WriteLineAsync("RufeInhaltAufAsync Thread:" + Thread.CurrentThread.ManagedThreadId);
+                return inhalt;
+            }
+        }
+    }
+
+
+
+}
 
 #region Task mit ContinueWith 
 //var meineTask =new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((daten)=>
@@ -13,10 +61,10 @@ Console.WriteLine("Hello, World!");
 #endregion
 
 #region Task ohne ContinueWith (Best practices)
-var meineTask = new HttpClient().GetStringAsync("https://www.google.com");
-Console.WriteLine("Zwischenarbeiten");
+//var meineTask = new HttpClient().GetStringAsync("https://www.google.com");
+//Console.WriteLine("Zwischenarbeiten");
 
-var daten = await meineTask;
-Console.WriteLine("Datengrösse:" + daten.Length);
+//var daten = await meineTask;
+//Console.WriteLine("Datengrösse:" + daten.Length);
 
 #endregion
