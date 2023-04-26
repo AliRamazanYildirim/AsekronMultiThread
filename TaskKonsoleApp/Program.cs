a3211d3b17a74403f393477af4e472f6fcf719bb
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-04-26 13:00:20 +0200

    Task Methode (WhenAny)

--- TaskKonsoleApp/Program.cs
+++ TaskKonsoleApp/Program.cs
@@ -1,6 +1,55 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
+#region Task WhenAll 
+//namespace TaskKonsoleApp
+//{
+//    public class Inhalt
+//    {
+//        public string? Seite { get; set; }
+//        public int Länge { get; set; }
 
+//    }
+//    internal class Programm
+//    {
+//        static async Task Main(string[] args)
+//        {
+//            await Console.Out.WriteLineAsync("Haupt Thread:" + Thread.CurrentThread.ManagedThreadId);
+//            List<string> urlListe = new List<string>()
+//            {
+//                 "https://www.google.com",
+//                 "https://www.microsoft.com",
+//                 "https://www.amazon.com",
+//                 "https://www.apple.com"
+//            };
+
+//            List<Task<Inhalt>> taskList = new List<Task<Inhalt>>();
+//            urlListe.ToList().ForEach(x =>
+//            {
+//                taskList.Add(RufeInhaltAufAsync(x));
+//            });
+
+//            var inhalte = await Task.WhenAll(taskList.ToArray());
+
+//            inhalte.ToList().ForEach(x =>
+//            {
+//                Console.WriteLine($"{x.Seite}- Grösse:{x.Länge}");
+//            });
+//            static async Task<Inhalt> RufeInhaltAufAsync(string url)
+//            {
+//                Inhalt inhalt = new Inhalt();
+//                var daten = await new HttpClient().GetStringAsync(url);
+
+//                inhalt.Seite = url;
+//                inhalt.Länge = daten.Length;
+//                await Console.Out.WriteLineAsync("RufeInhaltAufAsync Thread:" + Thread.CurrentThread.ManagedThreadId);
+//                return inhalt;
+//            }
+//        }
+//    }
+//}
+#endregion
+
+#region Task When Any
 namespace TaskKonsoleApp
 {
     public class Inhalt
@@ -27,13 +76,9 @@ namespace TaskKonsoleApp
             {
                 taskList.Add(RufeInhaltAufAsync(x));
             });
+            var ersteDatei = await Task.WhenAny(taskList);
+            await Console.Out.WriteLineAsync($"{ersteDatei.Result.Seite}-{ersteDatei.Result.Länge}");
 
-            var inhalte = await Task.WhenAll(taskList.ToArray());
-
-            inhalte.ToList().ForEach(x =>
-            {
-                Console.WriteLine($"{x.Seite}- Grösse:{x.Länge}");
-            });
             static async Task<Inhalt> RufeInhaltAufAsync(string url)
             {
                 Inhalt inhalt = new Inhalt();
@@ -46,10 +91,8 @@ namespace TaskKonsoleApp
             }
         }
     }
-
-
-
 }
+#endregion
 
 #region Task mit ContinueWith 
 //var meineTask =new HttpClient().GetStringAsync("https://www.google.com").ContinueWith((daten)=>
