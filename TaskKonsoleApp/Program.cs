2a62a97c54936a55c17090b845522e4cda01db70
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-04-27 21:13:22 +0200

    Task WitAll Methode

--- TaskKonsoleApp/Program.cs
+++ TaskKonsoleApp/Program.cs
@@ -49,7 +49,52 @@ using System.Diagnostics;
 //}
 #endregion
 
-#region Task When Any
+#region Task WhenAny
+//namespace TaskKonsoleApp
+//{
+//    public class Inhalt
+//    {
+//        public string? Seite { get; set; }
+//        public int Länge { get; set; }
+
+//    }
+//    internal class Programm
+//    {
+//        static async Task Main(string[] args)
+//        {
+//            await Console.Out.WriteLineAsync("Haupt Thread:" + Thread.CurrentThread.ManagedThreadId);
+//            List<string> urlListe = new List<string>()
+//            {
+//                 "https://www.google.com",
+//                 "https://www.microsoft.com",
+//                 "https://www.amazon.com",
+//                 "https://www.apple.com"
+//            };
+
+//            List<Task<Inhalt>> taskList = new List<Task<Inhalt>>();
+//            urlListe.ToList().ForEach(x =>
+//            {
+//                taskList.Add(RufeInhaltAufAsync(x));
+//            });
+//            var ersteDatei = await Task.WhenAny(taskList);
+//            await Console.Out.WriteLineAsync($"{ersteDatei.Result.Seite}-{ersteDatei.Result.Länge}");
+
+//            static async Task<Inhalt> RufeInhaltAufAsync(string url)
+//            {
+//                Inhalt inhalt = new Inhalt();
+//                var daten = await new HttpClient().GetStringAsync(url);
+
+//                inhalt.Seite = url;
+//                inhalt.Länge = daten.Length;
+//                await Console.Out.WriteLineAsync("RufeInhaltAufAsync Thread:" + Thread.CurrentThread.ManagedThreadId);
+//                return inhalt;
+//            }
+//        }
+//    }
+//}
+#endregion
+
+#region Task WaitAll
 namespace TaskKonsoleApp
 {
     public class Inhalt
@@ -76,8 +121,15 @@ namespace TaskKonsoleApp
             {
                 taskList.Add(RufeInhaltAufAsync(x));
             });
-            var ersteDatei = await Task.WhenAny(taskList);
-            await Console.Out.WriteLineAsync($"{ersteDatei.Result.Seite}-{ersteDatei.Result.Länge}");
+            await Console.Out.WriteLineAsync("Vor der WaitAll-Methode");
+
+            bool resultat= Task.WaitAll(taskList.ToArray(),5000);
+
+            Console.WriteLine("Ist das in 3 Sekunden gekommen?" + resultat);
+
+            await Console.Out.WriteLineAsync("Nach der WaitAll-Methode");
+
+            await Console.Out.WriteLineAsync($"{taskList.First().Result.Seite}-{taskList.First().Result.Länge}");
 
             static async Task<Inhalt> RufeInhaltAufAsync(string url)
             {
