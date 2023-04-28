837e4761f090645b7b56387d8a3f88579a03a254
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-04-28 18:28:48 +0200

    Task WaitAny Methode

--- TaskKonsoleApp/Program.cs
+++ TaskKonsoleApp/Program.cs
@@ -95,6 +95,58 @@ using System.Diagnostics;
 #endregion
 
 #region Task WaitAll
+//namespace TaskKonsoleApp
+//{
+//    public class Inhalt
+//    {
+//        public string? Seite { get; set; }
+//        public int Länge { get; set; }
+
+//    }
+//    internal class Programm
+//    {
+//        static async Task Main(string[] args)
+//        {
+//            await Console.Out.WriteLineAsync("Haupt Thread:" + Thread.CurrentThread.ManagedThreadId);
+//            List<string> urlListe = new List<string>()
+//            {
+//                 "https://www.google.com",
+//                 "https://www.microsoft.com",
+//                 "https://www.amazon.com",
+//                 "https://www.apple.com"
+//            };
+
+//            List<Task<Inhalt>> taskList = new List<Task<Inhalt>>();
+//            urlListe.ToList().ForEach(x =>
+//            {
+//                taskList.Add(RufeInhaltAufAsync(x));
+//            });
+//            await Console.Out.WriteLineAsync("Vor der WaitAll-Methode");
+
+//            bool resultat= Task.WaitAll(taskList.ToArray(),5000);
+
+//            Console.WriteLine("Ist das in 3 Sekunden gekommen?" + resultat);
+
+//            await Console.Out.WriteLineAsync("Nach der WaitAll-Methode");
+
+//            await Console.Out.WriteLineAsync($"{taskList.First().Result.Seite}-{taskList.First().Result.Länge}");
+
+//            static async Task<Inhalt> RufeInhaltAufAsync(string url)
+//            {
+//                Inhalt inhalt = new Inhalt();
+//                var daten = await new HttpClient().GetStringAsync(url);
+
+//                inhalt.Seite = url;
+//                inhalt.Länge = daten.Length;
+//                await Console.Out.WriteLineAsync("RufeInhaltAufAsync Thread:" + Thread.CurrentThread.ManagedThreadId);
+//                return inhalt;
+//            }
+//        }
+//    }
+//}
+#endregion
+
+#region Task WaitAny
 namespace TaskKonsoleApp
 {
     public class Inhalt
@@ -123,13 +175,15 @@ namespace TaskKonsoleApp
             });
             await Console.Out.WriteLineAsync("Vor der WaitAll-Methode");
 
-            bool resultat= Task.WaitAll(taskList.ToArray(),5000);
+            bool resultat = Task.WaitAll(taskList.ToArray(), 5000);
+
+            var ersterIndex = Task.WaitAny(taskList.ToArray());
 
             Console.WriteLine("Ist das in 3 Sekunden gekommen?" + resultat);
 
             await Console.Out.WriteLineAsync("Nach der WaitAll-Methode");
 
-            await Console.Out.WriteLineAsync($"{taskList.First().Result.Seite}-{taskList.First().Result.Länge}");
+            await Console.Out.WriteLineAsync($"{taskList[ersterIndex].Result.Seite}-{taskList[ersterIndex].Result.Länge}");
 
             static async Task<Inhalt> RufeInhaltAufAsync(string url)
             {
