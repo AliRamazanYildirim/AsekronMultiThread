6877d860af5b7b1be804f6d5d3214f72e2408270
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-15 20:46:36 +0200

    Parallel.ForEach Methode-2

--- ParallelTaskKonsoleApp/Program.cs
+++ ParallelTaskKonsoleApp/Program.cs
@@ -1,8 +1,57 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Diagnostics;
 #region Parallel.ForEach Mit System.Drawing.Image
-using System.Drawing;
+//using System.Drawing;
 
+//namespace ParallelTaskKonsoleApp
+//{
+//    internal class Program
+//    {
+//        private static void Main(string[] args)
+//        {
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+
+//            string bildWeg = @"C:\Users\Pro\TPL\";
+
+//            Directory.CreateDirectory(Path.Combine(bildWeg, "vorschauBild"));
+
+//            var daten = Directory.GetFiles(bildWeg);
+
+//            Parallel.ForEach(daten, (artikel) =>
+//            {
+//                Console.WriteLine("Threadnummer:" + Thread.CurrentThread.ManagedThreadId);
+//                Image image = new Bitmap(artikel);
+
+//                var vorschauBild = image.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+//                vorschauBild.Save(Path.Combine(bildWeg, "vorschauBild", Path.GetFileName(artikel)));
+//            });
+//            stopwatch.Stop();
+
+//            Console.WriteLine($"Der Vorgang wurde in {stopwatch.ElapsedMilliseconds} Sekunden beendet.");
+
+//            stopwatch.Restart();
+//            stopwatch.Start();
+
+//            daten.ToList().ForEach(a =>
+//            {
+//                Console.WriteLine("Threadnummer:" + Thread.CurrentThread.ManagedThreadId);
+//                Image image = new Bitmap(a);
+
+//                var vorschauBild = image.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+//                vorschauBild.Save(Path.Combine(bildWeg, "vorschauBild", Path.GetFileName(a)));
+//            });
+//            stopwatch.Stop();
+
+//            Console.WriteLine($"Der Vorgang wurde in {stopwatch.ElapsedMilliseconds} Sekunden beendet.");
+
+//        }
+
+//    }
+//}
+#endregion
+
+#region Parallel.ForEach Mit SixLabors.ImageSharp.Image
 namespace ParallelTaskKonsoleApp
 {
     internal class Program
@@ -21,13 +70,15 @@ namespace ParallelTaskKonsoleApp
             Parallel.ForEach(daten, (artikel) =>
             {
                 Console.WriteLine("Threadnummer:" + Thread.CurrentThread.ManagedThreadId);
-                Image image = new Bitmap(artikel);
 
-                var vorschauBild = image.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
-                vorschauBild.Save(Path.Combine(bildWeg, "vorschauBild", Path.GetFileName(artikel)));
+                var image = Image.Load(artikel);
+
+                image.Mutate(x => x.Resize(new ResizeOptions { Size = new Size(50, 50) }));
+                image.Save(Path.Combine(bildWeg, "vorschauBild", Path.GetFileName(artikel)));
+
             });
-            stopwatch.Stop();
 
+            stopwatch.Stop();
             Console.WriteLine($"Der Vorgang wurde in {stopwatch.ElapsedMilliseconds} Sekunden beendet.");
 
             stopwatch.Restart();
@@ -36,19 +87,17 @@ namespace ParallelTaskKonsoleApp
             daten.ToList().ForEach(a =>
             {
                 Console.WriteLine("Threadnummer:" + Thread.CurrentThread.ManagedThreadId);
-                Image image = new Bitmap(a);
 
-                var vorschauBild = image.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
-                vorschauBild.Save(Path.Combine(bildWeg, "vorschauBild", Path.GetFileName(a)));
+                var image = Image.Load(a);
+
+                image.Mutate(x => x.Resize(new ResizeOptions { Size = new Size(50, 50) }));
+                image.Save(Path.Combine(bildWeg, "vorschauBild", Path.GetFileName(a)));
+
             });
+
             stopwatch.Stop();
-
             Console.WriteLine($"Der Vorgang wurde in {stopwatch.ElapsedMilliseconds} Sekunden beendet.");
-
         }
-
     }
 }
 #endregion
-
-
