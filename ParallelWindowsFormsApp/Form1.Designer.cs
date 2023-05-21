d5e3269d33e30e374e71452ca4bbbae29e96f0a1
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-21 14:24:02 +0200

    Parallel.ForEach CancellationToken

---
+++ ParallelWindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace ParallelWindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(44, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Aufrufen";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(44, 38);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(156, 134);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(125, 178);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Erhöhen";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(87, 207);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Abbrechen";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ ParallelWindowsFormsApp/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ParallelWindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        CancellationTokenSource _cancellationToken;
+        private int theke { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            _cancellationToken=new CancellationTokenSource();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _cancellationToken = new CancellationTokenSource();
+            List<string>urls=new List<string>()
+            {
+                "https://www.google.com",
+                "https://www.amazon.com",
+                "https://www.microsoft.com",
+                "https://www.lenovo.com",
+                "https://www.dell.com",
+
+            };
+            HttpClient httpClient = new HttpClient();
+            ParallelOptions parallelOptions = new ParallelOptions();
+            parallelOptions.CancellationToken = _cancellationToken.Token;
+            Task.Run(() =>
+            {
+                try
+                {
+                    Parallel.ForEach<string>(urls, parallelOptions, (url) =>
+                    {
+                        string inhalt = httpClient.GetStringAsync(url).Result;
+                        string datei = $"{url}:{inhalt.Length}";
+                        _cancellationToken.Token.ThrowIfCancellationRequested();
+
+                        listBox1.Invoke((MethodInvoker)delegate
+                        {
+                            listBox1.Items.Add(datei);
+                        });
+
+                    });
+                }
+                catch (OperationCanceledException ex)
+                {
+
+                    MessageBox.Show("Der Vorgang wurde abgebrochen." + ex.Message);
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show("Der Vorgang wurde beendet" + ex.Message);
+                }
+
+            });
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            _cancellationToken.Cancel();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button2.Text = theke++.ToString();
+        }
+    }
+}
