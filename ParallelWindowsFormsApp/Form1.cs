88ebded71a42dead0481d062fbd4fdabc368e2e8
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-21 21:35:38 +0200

    Parallel.ForEach CancellationToken-2

--- ParallelWindowsFormsApp/Form1.cs
+++ ParallelWindowsFormsApp/Form1.cs
@@ -10,6 +10,80 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+#region Parallel.ForEach CancellationToken-1
+//namespace ParallelWindowsFormsApp
+//{
+//    public partial class Form1 : Form
+//    {
+//        CancellationTokenSource _cancellationToken;
+//        private int theke { get; set; } = 0;
+//        public Form1()
+//        {
+//            InitializeComponent();
+//            _cancellationToken = new CancellationTokenSource();
+//        }
+
+//        private void button1_Click(object sender, EventArgs e)
+//        {
+//            _cancellationToken = new CancellationTokenSource();
+//            List<string> urls = new List<string>()
+//            {
+//                "https://www.google.com",
+//                "https://www.amazon.com",
+//                "https://www.microsoft.com",
+//                "https://www.lenovo.com",
+//                "https://www.dell.com",
+
+//            };
+//            HttpClient httpClient = new HttpClient();
+//            ParallelOptions parallelOptions = new ParallelOptions();
+//            parallelOptions.CancellationToken = _cancellationToken.Token;
+//            Task.Run(() =>
+//            {
+//                try
+//                {
+//                    Parallel.ForEach<string>(urls, parallelOptions, (url) =>
+//                    {
+//                        string inhalt = httpClient.GetStringAsync(url).Result;
+//                        string datei = $"{url}:{inhalt.Length}";
+//                        _cancellationToken.Token.ThrowIfCancellationRequested();
+
+//                        listBox1.Invoke((MethodInvoker)delegate
+//                        {
+//                            listBox1.Items.Add(datei);
+//                        });
+
+//                    });
+//                }
+//                catch (OperationCanceledException ex)
+//                {
+
+//                    MessageBox.Show("Der Vorgang wurde abgebrochen." + ex.Message);
+//                }
+//                catch (Exception ex)
+//                {
+
+//                    MessageBox.Show("Der Vorgang wurde beendet" + ex.Message);
+//                }
+
+//            });
+
+//        }
+
+//        private void button3_Click(object sender, EventArgs e)
+//        {
+//            _cancellationToken.Cancel();
+//        }
+
+//        private void button2_Click(object sender, EventArgs e)
+//        {
+//            button2.Text = theke++.ToString();
+//        }
+//    }
+//}
+#endregion
+
+#region Parallel.ForEach CancellationToken-2
 namespace ParallelWindowsFormsApp
 {
     public partial class Form1 : Form
@@ -45,7 +119,7 @@ namespace ParallelWindowsFormsApp
                     {
                         string inhalt = httpClient.GetStringAsync(url).Result;
                         string datei = $"{url}:{inhalt.Length}";
-                        _cancellationToken.Token.ThrowIfCancellationRequested();
+                        parallelOptions.CancellationToken.ThrowIfCancellationRequested();
 
                         listBox1.Invoke((MethodInvoker)delegate
                         {
@@ -80,3 +154,5 @@ namespace ParallelWindowsFormsApp
         }
     }
 }
+
+#endregion
