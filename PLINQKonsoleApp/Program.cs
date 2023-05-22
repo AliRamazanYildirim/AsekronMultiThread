a52137c7e6379d78c899b8092a14c6796d993160
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-22 22:13:33 +0200

    AdventureWorks2019 Database First

--- PLINQKonsoleApp/Program.cs
+++ PLINQKonsoleApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using PLINQKonsoleApp.Modelle;
 using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 #region PLINQ AsParallel Methode
@@ -36,35 +37,52 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 #endregion
 
 #region PLINQ ForAll Methode
+//namespace TaskKonsoleApp
+//{
+//    class Program
+//    {
+//        private static bool Vorgang(int nummer)
+//        {
+//            return nummer % 2 == 0;
+//        }
+//        private static void Main(string[] args)
+//        {
+
+//            var array = Enumerable.Range(1, 100).ToList();
+
+//            var neueArray = array.AsParallel().Where(Vorgang);
+
+
+//            //neueArray.ToList().ForEach(nummer =>
+//            //{
+//            //    Thread.Sleep(500);
+//            //    Console.WriteLine(nummer);
+//            //});
+
+//            #region Mit ForAll
+//            neueArray.ForAll(nummer =>
+//            {
+//                Thread.Sleep(500);
+//                Console.WriteLine($"Nummer {nummer} wurde mit Thread: {Thread.CurrentThread.ManagedThreadId} gerechnet.");
+//            });
+//            #endregion
+//        }
+//    }
+//}
+#endregion
+
+#region AdventureWorks2019 Database First
 namespace TaskKonsoleApp
 {
     class Program
     {
-        private static bool Vorgang(int nummer)
-        {
-            return nummer % 2 == 0;
-        }
         private static void Main(string[] args)
         {
-
-            var array = Enumerable.Range(1, 100).ToList();
-
-            var neueArray = array.AsParallel().Where(Vorgang);
-
-
-            //neueArray.ToList().ForEach(nummer =>
-            //{
-            //    Thread.Sleep(500);
-            //    Console.WriteLine(nummer);
-            //});
-
-            #region Mit ForAll
-            neueArray.ForAll(nummer =>
+            AdventureWorks2019Context kontext = new AdventureWorks2019Context();
+            kontext.Products.Take(10).ToList().ForEach(p =>
             {
-                Thread.Sleep(500);
-                Console.WriteLine($"Nummer {nummer} wurde mit Thread: {Thread.CurrentThread.ManagedThreadId} gerechnet.");
+                Console.WriteLine(p.Name);
             });
-            #endregion
         }
     }
 }
