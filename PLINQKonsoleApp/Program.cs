e1c523210655c1028ef9c0a85b293dc456bc362a
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-22 17:44:03 +0200

    PLINQ AsParallel Methode

---
+++ PLINQKonsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace TaskKonsoleApp
+{
+    class Program
+    {
+        private static bool Vorgang(int nummer)
+        {
+            return nummer % 2 == 0;
+        }
+        private  static void Main(string[] args)
+        {
+            var stringBuilder = new StringBuilder();
+
+            var array = Enumerable.Range(1, 100).ToList();
+
+            array.Where(Vorgang).ToList().ForEach(nummer =>
+            {
+                stringBuilder.AppendLine(nummer.ToString());
+            });
+            Console.WriteLine(stringBuilder);
+
+            var neueArray = array.AsParallel().Where(nummer => nummer % 2 == 0);
+
+
+            neueArray.ToList().ForEach(nummer =>
+            {
+                stringBuilder.AppendLine(nummer.ToString());
+            });
+            Console.WriteLine(stringBuilder);
+        }
+    }
+}
