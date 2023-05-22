0df42d585150781ba8ec9e4346d4a08b65017ef2
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-22 19:31:45 +0200

    PLINQ ForAll Methode

--- PLINQKonsoleApp/Program.cs
+++ PLINQKonsoleApp/Program.cs
@@ -1,7 +1,41 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
+#region PLINQ AsParallel Methode
+//namespace TaskKonsoleApp
+//{
+//    class Program
+//    {
+//        private static bool Vorgang(int nummer)
+//        {
+//            return nummer % 2 == 0;
+//        }
+//        private static void Main(string[] args)
+//        {
+//            var stringBuilder = new StringBuilder();
 
+//            var array = Enumerable.Range(1, 100).ToList();
+
+//            array.Where(Vorgang).ToList().ForEach(nummer =>
+//            {
+//                stringBuilder.AppendLine(nummer.ToString());
+//            });
+//            Console.WriteLine(stringBuilder);
+
+//            var neueArray = array.AsParallel().Where(nummer => nummer % 2 == 0);
+
+
+//            neueArray.ToList().ForEach(nummer =>
+//            {
+//                stringBuilder.AppendLine(nummer.ToString());
+//            });
+//            Console.WriteLine(stringBuilder);
+//        }
+//    }
+//}
+#endregion
+
+#region PLINQ ForAll Methode
 namespace TaskKonsoleApp
 {
     class Program
@@ -10,26 +44,28 @@ namespace TaskKonsoleApp
         {
             return nummer % 2 == 0;
         }
-        private  static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            var stringBuilder = new StringBuilder();
 
             var array = Enumerable.Range(1, 100).ToList();
 
-            array.Where(Vorgang).ToList().ForEach(nummer =>
+            var neueArray = array.AsParallel().Where(Vorgang);
+
+
+            //neueArray.ToList().ForEach(nummer =>
+            //{
+            //    Thread.Sleep(500);
+            //    Console.WriteLine(nummer);
+            //});
+
+            #region Mit ForAll
+            neueArray.ForAll(nummer =>
             {
-                stringBuilder.AppendLine(nummer.ToString());
+                Thread.Sleep(500);
+                Console.WriteLine($"Nummer {nummer} wurde mit Thread: {Thread.CurrentThread.ManagedThreadId} gerechnet.");
             });
-            Console.WriteLine(stringBuilder);
-
-            var neueArray = array.AsParallel().Where(nummer => nummer % 2 == 0);
-
-
-            neueArray.ToList().ForEach(nummer =>
-            {
-                stringBuilder.AppendLine(nummer.ToString());
-            });
-            Console.WriteLine(stringBuilder);
+            #endregion
         }
     }
 }
+#endregion
