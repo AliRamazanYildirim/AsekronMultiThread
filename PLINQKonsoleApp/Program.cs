287084630912452aa0c8fcb3c2e02fd50f101f19
aliramazanyildirim <aliramazanyildirim@gmail.com>
2023-05-24 16:27:06 +0200

    PLINQ WithExecuteMode Methode

--- PLINQKonsoleApp/Program.cs
+++ PLINQKonsoleApp/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using PLINQKonsoleApp.Modelle;
+using System.Diagnostics;
 using System.Runtime.ConstrainedExecution;
 using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
@@ -132,22 +133,81 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 #endregion
 
 #region PLINQ WithDegreeOfParalleism Methode
+//namespace TaskKonsoleApp
+//{
+//    class Program
+//    {
+//        private static void SchreibenProtokollierung(Product p)
+//        {
+//            Console.WriteLine(p.Name + $" wurde mit {Thread.CurrentThread.ManagedThreadId}. Thread protokolliert.");
+//        }
+//        private static void Main(string[] args)
+//        {
+//            AdventureWorks2019Context kontext = new AdventureWorks2019Context();
+
+//            kontext.Products.AsParallel().WithDegreeOfParallelism(2).ForAll(p =>
+//            {
+//                SchreibenProtokollierung(p);
+//            });
+//        }
+//    }
+//}
+#endregion
+
+#region PLINQ WithExecuteMode Methode
+//namespace TaskKonsoleApp
+//{
+//    class Program
+//    {
+//        private static void SchreibenProtokollierung(Product p)
+//        {
+//            Console.WriteLine(p.Name + $" wurde mit {Thread.CurrentThread.ManagedThreadId}. Thread protokolliert.");
+//        }
+//        private static void Main(string[] args)
+//        {
+//            Stopwatch stopwatch = new Stopwatch();
+//            stopwatch.Start();
+//            AdventureWorks2019Context kontext = new AdventureWorks2019Context();
+
+//            #region ParallelExecutionMode.Default
+//            kontext.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.Default).Where(p => p.ListPrice > 10M).Take(10).ForAll(p =>
+//                {
+//                    SchreibenProtokollierung(p);
+//                });
+//            stopwatch.Stop();
+//            Console.WriteLine($"Standard-Betriebsdauer wurde in {stopwatch.ElapsedMilliseconds} Millisekunden ausgeführt.");
+//            #endregion
+
+//            #region ParallelExecutionMode.ForceParallelism
+//            //kontext.Products.AsParallel().WithExecutionMode(ParallelExecutionMode.ForceParallelism).Where(p => p.ListPrice > 10M).Take(10).ForAll(p =>
+//            //{
+//            //    SchreibenProtokollierung(p);
+//            //});
+//            //stopwatch.Stop();
+//            //Console.WriteLine($"ForceParallelism-Betriebsdauer wurde in {stopwatch.ElapsedMilliseconds} Millisekunden ausgeführt.");
+//            #endregion
+//        }
+//    }
+//}
+#endregion
+
+#region PLINQ AsOrdered Methode
 namespace TaskKonsoleApp
 {
     class Program
     {
-        private static void SchreibenProtokollierung(Product p)
-        {
-            Console.WriteLine(p.Name + $" wurde mit {Thread.CurrentThread.ManagedThreadId}. Thread protokolliert.");
-        }
         private static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
             AdventureWorks2019Context kontext = new AdventureWorks2019Context();
-
-            kontext.Products.AsParallel().WithDegreeOfParallelism(2).ForAll(p =>
-            {
-                SchreibenProtokollierung(p);
-            });
+            
+            kontext.Products.AsParallel().AsOrdered().Where(p => p.ListPrice > 10M).ToList().ForEach(p =>
+                {
+                    Console.WriteLine($"Produktname:{p.Name} - Produktpreis:{p.ListPrice}");
+                });
+            stopwatch.Stop();
+            Console.WriteLine($"Der Vorgang wurde in {stopwatch.ElapsedMilliseconds} Millisekunden ausgeführt.");           
         }
     }
 }
